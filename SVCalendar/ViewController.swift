d3b6c8ab9ad804b94c0a58179579fdc99b59df6e
satish <satish.v@canopas.com>
2018-10-22 17:47:30 +0530

    project init

---
+++ SVCalendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SVCalendar
+//
+//  Created by Canopas on 22/10/18.
+//  Copyright © 2018 Canopas Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
